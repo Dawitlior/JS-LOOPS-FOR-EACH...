5ce7e4c1f3b77a024472ca71ccc91a04cd330312
Dawitlior <dawitlior777@gmail.com>
2022-06-16 11:00:22 +0300

    ADD

---
+++ main.js
@@ -0,0 +1,15 @@
+// שימוש בלולאת פור איץ
+//לא עושה שינוי במערך המקורי
+// פונקציית VOID היא לא מחזירה לנו ערך
+const colors = ["red", "green", "blue", "black"];
+colors.forEach((colorItem) => {
+  console.log(colorItem.toLocaleUpperCase());
+});
+
+
+
+const colors1 = ['brown','red','yellow','green']
+
+colors1.forEach((item)=>{
+item.toUpperCase()
+})
